b233be7f7dd4ba5ab86969c640a6f64954d70a88
HurjaMur <153192124+HurjaMur@users.noreply.github.com>
2024-02-18 23:48:03 +0300

    Update orchestrator.go

--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -1,44 +1,55 @@
 package orchestrator
 
 import (
-	"math/rand"
-	"regexp"
-
 	"github.com/HurjaMur/projectcalcversion2/agent"
 	"github.com/HurjaMur/projectcalcversion2/data"
 )
 
 type Base struct {
 	Expression string
-	ID         int
-}
-
-func GenerateID() int {
-	return rand.Intn(1000000) // Генерим случайный айди
 }
 
 func NewOrchestrator(expression string) Base {
 	return Base{
 		Expression: expression,
-		ID:         GenerateID(),
 	}
+}
+
+func isExpressionValid(expr string) bool {
+	if len(expr) == 0 {
+		return false
+	}
+
+	return isValidExpression(expr, 0)
+}
+
+func isValidExpression(expr string, index int) bool {
+	if index == len(expr) {
+		return true
+	}
+
+	if expr[index] >= '0' && expr[index] <= '9' {
+		return isValidExpression(expr, index+1)
+	} else if expr[index] == '+' || expr[index] == '-' || expr[index] == '*' || expr[index] == '/' {
+		return isValidExpression(expr, index+1)
+	}
+
+	return false
 }
 
 func (b Base) ParseAndProcessExpression() (string, error) {
 	expression := b.Expression
-
-	// Валидность выражения
-	validChars := regexp.MustCompile(`^[0-9\+\-\*\/\s()]+$`)
-
-	// Проверка валидности выражения
-	if !validChars.MatchString(expression) {
+	if !isExpressionValid(expression) { //Проверяем на валидность
 		return "400. Выражение невалидно", nil
 	}
 
-	result := agent.ProcessExpression(expression)
-
-	// Сохранение результата в базу данных
-	data.SaveToDatabase(b.ID, expression, result)
+	if !isValidExpression(expression, 0) { //Проверяем на валидность
+		return "400. Выражение невалидно", nil
+	}
+	if isValidExpression(expression, 0) && isExpressionValid(expression) {
+		result := agent.ProcessExpression(expression) //Считаем с помощью агента
+		data.SaveToDatabase(expression, result)       //Сохраняем в базу данных
+	}
 
 	return "200. Выражение успешно принято, распаршено и принято к обработке", nil
 }
