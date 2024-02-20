177df406a971e8e1840be27a18ec64bdc4a728a3
HurjaMur <153192124+HurjaMur@users.noreply.github.com>
2024-02-20 17:03:37 +0300

    Update main.go

--- main/main.go
+++ main/main.go
@@ -10,12 +10,15 @@ import (
 
 func main() {
 	var expression string
+	var timee int
 	fmt.Print("Введите ваше выражение")
-	x, _ := fmt.Scan(&expression)                    // Введите ваше выражение
+	x, _ := fmt.Scan(&expression) // Введите ваше выражение
+	fmt.Print("Введите время операции")
+	y, _ := fmt.Scan(&timee)                         // Введите время обработки операции
 	orch := orchestrator.NewOrchestrator(expression) //Добавляем наше выражение в оркестратор
 	exp, _ := orch.ParseAndProcessExpression()
-	time.Sleep(1 * time.Second) // Введите сколько времени должно пройти до вычисления
-	x++                         //Просто юзаем, чтобы не было ошибок в компиляторе
+	time.Sleep(time.Duration(timee) * time.Second) // Введите сколько времени должно пройти до вычисления
+	x += y                                         //Просто юзаем, чтобы не было ошибок в компиляторе
 	res := data.ExpressionResultMap[fmt.Sprint(expression)]
 	fmt.Printf("Корректность вашего выражения: %v, Ответ на ваше выражение: %v\n", exp, res)
 }
